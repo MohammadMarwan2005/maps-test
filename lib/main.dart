23421c2976b3df7bf0ed7d793ad132f11da3fd58
Mohammad Al-Aishat <alaishatmohammad@gmail.com>
2025-07-16 15:49:18 +0300

    🚩 Removed the debug mode banner 😄

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: FirstScreen());
+    return MaterialApp(debugShowCheckedModeBanner: false, home: FirstScreen());
   }
 }
 
